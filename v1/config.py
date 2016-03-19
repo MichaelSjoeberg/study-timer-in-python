921e3fad029bb64174229448748cc1bab638c634
Michael Sjoeberg <mail@michaelsjoeberg.com>
2016-03-20 00:52:57 +0100

    Update
    Separated functions to separate files, added a demo txt-file for
    displaying diagram, exception handling for inputs.

---
+++ v1/config.py
@@ -0,0 +1,18 @@
+
+# function for subjects
+def subjects():
+	"Create dictionary with subjects."
+
+	subject_dict = {
+		'1'	: 'Mathematics',
+		'2'	: 'Programming (Python)',
+		'3'	: 'Programming (Java)',
+		'4'	: 'Programming (Other)',
+		'5'	: 'Web Development',
+		'6'	: 'Finance (MS Excel)',
+		'7'	: 'Finance (General)',
+		'8'	: 'Languages (German)',
+		'9'	: 'Languages (Norwegian)', 
+		'10': 'Languages (Other)'}
+		
+	return subject_dict
--- v1/timer.py
+++ v1/timer.py
@@ -1,13 +1,29 @@
-# timer for recording study times to a txt-file.
-
+# import dependencies
+import sys, traceback
 from timeit import default_timer
-from pylab import *
 import time
 
-###### FUNCTIONS ######
+# import config
+from config import *
 
+# import function for diagram
+import diagram
+
+subject_dict = {
+		'1'	: 'Mathematics',
+		'2'	: 'Programming (Python)',
+		'3'	: 'Programming (Java)',
+		'4'	: 'Programming (Other)',
+		'5'	: 'Web Development',
+		'6'	: 'Finance (MS Excel)',
+		'7'	: 'Finance (General)',
+		'8'	: 'Languages (German)',
+		'9'	: 'Languages (Norwegian)', 
+		'10': 'Languages (Other)'}
+
+# function for stopping and recording time
 def stopTimer(s,t):
-	"take snapshot of system time when entering 'stop' in terminal"
+	"Take a snapshot of system time and write total time to file."
 
 	stop = raw_input("\nEnter 'stop' to stop timer.\n\n" + ">\t")
 
@@ -25,114 +41,66 @@ def stopTimer(s,t):
 	if again == "" or len(again) > 0:
 		query()
 
+# function for displaying subjects and getting input 
 def query():
-	"ask user for input"
+	"Display subjects and ask user for input, then validate input."
 
-	print "\n\n------------------- Study Timer -------------------\n" + "Subjects:\n"
+	try:
+		print "\n\n------------------- Study Timer -------------------\n" + "Subjects:\n"
 
-	n = 1
-	for s in subject_dict:
-		print "\t" + str(n) + ". " + subject_dict[str(n)] + "\n"
-		n = n + 1
-	print "---------------- By Michael Sjoeberg ----------------\n"
+		# display all subjects in imported subject dictionnary
+		n = 1
+		for s in subject_dict:
+			print "\t" + str(n) + ". " + subject_dict[str(n)] + "\n"
+			n = n + 1
+		print "---------------- By Michael Sjoeberg ----------------\n"
 
-	subject = raw_input("What are we studying today (press ENTER for Diagram)?\n\n" + ">\t")
+		subject = raw_input("What are we studying today (press ENTER for Diagram)?\n\n" + ">\t")
 
-	checkQuery(subject)
+		# validate input
+		checkQuery(subject)
 
+	except KeyboardInterrupt:
+		print "\n\nExiting...  See you!\n"
+
+	except Exception:
+		traceback.print_exc(file=sys.stdout)
+
+	sys.exit(0)
+
+# function for validating input from 'query()'
 def checkQuery(s):
-	"check user input"
+	"Display diagram if input is blank, start timer if input is correct."
 
-	# show diagram when empty input
-	if s == '':
-		createDiagram()
-
-	elif int(s) > 0 and int(s) <= len(subject_dict):
-		check = raw_input("\nYou're studying " + subject_dict[str(s)] + "." + " Is that correct? (Y/N)" + ">\t")
-		if check == "Y" or check == "y":
-			start = default_timer()
-			print "\nOK! Good luck!"
-			stopTimer(s,start)
-		if check == "N" or check == "n":
-			print "Let's try again..."
+	try:
+		# display diagram if input is blank
+		if s == '':
+			diagram.createDiagram(subject_dict)
 			query()
-		else:
-			"Please enter Y or N."
-			checkQuery(s)
 
-	return
+		# validate input and act accordingly
+		elif int(s) > 0 and int(s) <= len(subject_dict):
+			check = raw_input("\nYou're studying " + subject_dict[str(s)] + "." + " Is that correct? (Y/N)" + ">\t")
+			
+			# start timer if correct
+			if check == "Y" or check == "y":
+				start = default_timer()
+				print "\nOK! Good luck!"
+				stopTimer(s,start)
 
-def createDiagram():
-	"collect data and create a diagram"
+			# ask for subject again if incorrect
+			if check == "N" or check == "n":
+				print "Let's try again..."
+				query()
 
-	total_time = 0
+			# ask again if none of above
+			else:
+				"Please enter Y or N."
+				checkQuery(s)
 
-	# empty dictionary
-	data_dict = {}
+	except Exception:
+		print "\n\nNot a valid input, please try again.\n"
+		query()
 
-	for s in subject_dict:
-
-		# initialise total for key
-		data_dict[s] = 0
-
-		# open txt-file
-		timer_file = open(time.strftime("%B:%Y") + ".txt", "r")
-
-		# split lines into sections
-		for line in timer_file:
-
-			# find and add math times
-			if line.startswith(subject_dict[str(s)]):
-				section = line.split(":")
-				t = section[1]
-				t = t[:-2]
-				
-				# add to total
-				data_dict[s] = float(data_dict[s]) + float(t)
-				total_time = total_time + float(t)
-
-	# make a square figure and axes
-	figure(1, figsize=(10,8))
-	ax = axes([0.1, 0.1, 0.8, 0.8])
-	ax.set_aspect('equal')
-
-	# The slices will be ordered and plotted counter-clockwise.
-	labels = ()
-	fracs = ()
-
-	for s in data_dict:
-		if data_dict[str(s)] == 0:
-			continue
-		else:
-			labels = labels + (subject_dict[str(s)],)
-			fracs = fracs + (data_dict[str(s)] / total_time,)
-
-	colors = ['yellowgreen', 'lightskyblue', 'lightcoral', 'aliceblue', 'seagreen', 'turquoise' ,'papayawhip', 'saddlebrown', 'powderblue', 'plum']
-	# explode = (0, 0, 0, 0, 0, 0, 0)
-
-	pie(fracs, labels=labels, colors=colors,
-	                autopct='%1.1f%%', shadow=False, startangle=90)
-	                # The default startangle is 0, which would start
-	                # the Frogs slice on the x-axis.  With startangle=90,
-	                # everything is rotated counter-clockwise by 90 degrees,
-	                # so the plotting starts on the positive y-axis.
-
-	title('Study time distribution', bbox={'facecolor':'1', 'pad':28})
-	show()
-	query()
-
-##### END FUNCTIONS #####
-
-subject_dict = {
-	'1'	: 'Mathematics',
-	'2'	: 'Programming (Python)',
-	'3'	: 'Programming (Java)',
-	'4'	: 'Programming (Other)',
-	'5'	: 'Web Development',
-	'6'	: 'Finance (MS Excel)',
-	'7'	: 'Finance (General)',
-	'8'	: 'Languages (German)',
-	'9'	: 'Languages (Norwegian)', 
-	'10': 'Languages (Other)'}
-
+# initialise program
 query()
