8c64159d1dc22fd5aa799aa6d6fb9f8b5474b1c5
Michael Sjoeberg <mail@michaelsjoeberg.com>
2016-04-05 11:48:49 +0200

    Migrated 'Michael' to new version

---
+++ Michael/diagram.py
@@ -0,0 +1,71 @@
+# import dependencies
+import sys, traceback
+from timeit import default_timer
+import time
+from pylab import *
+
+# function for creating diagram
+def createDiagram(subject_dict, month):
+	"Collect data from file and create a diagram."
+
+	total_time = 0
+
+	# empty dictionary
+	data_dict = {}
+
+	for s in subject_dict:
+
+		# initialise total for key
+		data_dict[s] = 0
+
+		# open txt-file based on time, requires time plugins
+		timer_file = open(month + time.strftime(":%Y") + ".txt", "r")
+
+		# open demo txt-file
+		# timer_file = open("demo" + ".txt", "r")
+
+		# split lines into sections
+		for line in timer_file:
+
+			# find and add math times
+			if subject_dict[str(s)] in line:
+				section = line.split(":")
+				t = section[1]
+				t = t[:-2]
+				
+				# add to total
+				data_dict[s] = float(data_dict[s]) + float(t)
+				total_time = total_time + float(t)
+
+		timer_file.close()
+
+	# make a square figure and axes
+	figure(1, figsize=(10,8))
+	ax = axes([0.1, 0.1, 0.8, 0.8])
+	ax.set_aspect('equal')
+
+	# The slices will be ordered and plotted counter-clockwise.
+	labels = ()
+	fracs = ()
+
+	for s in data_dict:
+		if data_dict[str(s)] == 0:
+			continue
+		else:
+			labels = labels + (subject_dict[str(s)],)
+			fracs = fracs + (data_dict[str(s)] / total_time,)
+
+	colors = ['yellowgreen', 'lightskyblue', 'lightcoral', 'aliceblue', 'seagreen', 'turquoise' ,'papayawhip', 'saddlebrown', 'powderblue', 'plum']
+	# explode = (0, 0, 0, 0, 0, 0, 0)
+
+	pie(fracs, labels=labels, colors=colors,
+	                autopct='%1.1f%%', shadow=False, startangle=90)
+	                # The default startangle is 0, which would start
+	                # the Frogs slice on the x-axis.  With startangle=90,
+	                # everything is rotated counter-clockwise by 90 degrees,
+	                # so the plotting starts on the positive y-axis.
+
+	title("Study timer for " + month + " " + time.strftime("%Y"), bbox={'facecolor':'1', 'pad':28, 'alpha':0})
+	show()
+
+	return
---
+++ Michael/functions.py
@@ -0,0 +1,98 @@
+# import dependencies
+import sys, traceback
+from timeit import default_timer
+import time
+
+# import config and diagram modules
+import config, diagram
+
+subject_dict = config.subjects()
+
+# function for stopping and recording time
+def stopTimer(s,t):
+	"Take a snapshot of system time and write total time to file."
+
+	while raw_input("\nEnter 'stop' to stop timer.\n\n" + ">\t") != "stop":
+		# keep asking
+		continue
+
+	# calculate study time
+	duration = default_timer() - t
+
+	# create new or append existing txt-file to store data
+	file = open(time.strftime("%B:%Y") + ".txt", "a")
+	file.write(time.strftime("%a %d") + " - " + subject_dict[s] + ": " + str(duration / 60) + "\n")
+	file.close()
+
+	print "\nGood job! You studied " + subject_dict[s] + " for a total of " + str(int(duration / 60)) + " minutes."
+
+	# type enter to continue
+	again = raw_input("\nPress ENTER to continue.\n\n" + ">\t")
+	if again == "" or len(again) > 0:
+		query(s)
+
+# function for displaying subjects and getting input 
+def query():
+	"Display subjects and ask user for input, then validate input."
+
+	try:
+		print "\n\n------------------- Study Timer -------------------\n" + "Subjects:\n"
+
+		# display all subjects in imported subject dictionnary
+		n = 1
+		for s in subject_dict:
+			print "\t" + str(n) + ". " + subject_dict[str(n)] + "\n"
+			n = n + 1
+		print "---------------- By Michael Sjoeberg ----------------\n"
+		subject = raw_input("What are we studying today (press enter for current month diagram)?\n\n" + ">\t")
+
+		# validate input
+		checkQuery(subject)
+
+	except KeyboardInterrupt:
+		print "\n\nExiting...  See you!\n"
+
+	except Exception:
+		traceback.print_exc(file=sys.stdout)
+
+	sys.exit(0)
+
+# function for validating input from 'query()'
+def checkQuery(s):
+	"Display diagram if input is blank, start timer if input is correct."
+
+	try:
+		# display diagram if input is blank
+		if s == '':
+			diagram.createDiagram(subject_dict, time.strftime("%B"))
+			query()
+
+		elif s == 'January' or s == 'February' or s == 'March' or s == 'April' or s == 'May' or s == 'June' or s == 'July' or s == 'August' or s == 'September' or s == 'October' or s == 'November' or s == 'December':
+			diagram.createDiagram(subject_dict, s)
+			query()
+
+		# validate input and act accordingly
+		elif int(s) > 0 and int(s) <= len(subject_dict):
+			check = raw_input("\nYou're studying " + subject_dict[str(s)] + "." + " Is that correct? (Y/N)" + ">\t")
+			
+			# start timer if correct
+			if check == "Y" or check == "y":
+				start = default_timer()
+				print "\nOK! Good luck!"
+				stopTimer(s,start)
+
+			# ask for subject again if incorrect
+			if check == "N" or check == "n":
+				print "Let's try again..."
+				query()
+
+			# ask again if none of above
+			else:
+				"Please enter Y or N."
+				checkQuery(s)
+
+	except Exception:
+		print "\n\nNot a valid input, please try again.\n"
+		query()
+
+
